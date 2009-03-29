a09c65c0e42c9f72edc1ecdc3fb45dafcedf0f1a
mstine <matt@mattstine.com>
2009-03-29 02:02:51 -0500

    tweak with Glen's suggestions

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,32 +1,41 @@
-dataSource {
+/*dataSource {
 	pooled = true
 	driverClassName = "com.mysql.jdbc.Driver"
 	username = "root"
 	password = ""
-}
+}*/
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost/wendysStoreDev"
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:mysql://localhost/wendysStoreTest"
-		}
-	}
-	production {
-		dataSource {
-            dbCreate = "update"
-		    jndiName = "java:jdbc/WendysStoreProdDB"
-		}
-	}
+  development {
+    dataSource {
+      pooled = true
+      driverClassName = "com.mysql.jdbc.Driver"
+      username = "root"
+      password = ""
+      dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+      url = "jdbc:mysql://localhost/wendysStoreDev"
+    }
+  }
+  test {
+    dataSource {
+      pooled = true
+      driverClassName = "com.mysql.jdbc.Driver"
+      username = "root"
+      password = ""
+      dbCreate = "update"
+      url = "jdbc:mysql://localhost/wendysStoreTest"
+    }
+  }
+  production {
+    dataSource {
+      pooled = false
+      dbCreate = "update"
+      jndiName = "java:comp/env/jdbc/WendysStoreProdDB"
+    }
+  }
 }
