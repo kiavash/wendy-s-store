13fddb5b4ae84451d6c63cbf054f8944f44ef83f
mstine <matt@mattstine.com>
2009-04-09 20:09:20 -0500

    taglib for rendering customizations in different contexts

---
+++ grails-app/taglib/com/mattstine/wendysstore/taglib/RenderCustomizationWidgetTagLib.groovy
@@ -0,0 +1,33 @@
+package com.mattstine.wendysstore.taglib
+
+import com.mattstine.wendysstore.domain.CustomizationType
+
+class RenderCustomizationWidgetTagLib {
+
+  def renderCustomizationWidget = {attrs, body ->
+    out << render(template: "/${attrs.customization.type.widgetName}", model: [customization: attrs.customization])
+  }
+
+  def renderCartCustomizationItem = {attrs, body ->
+    def customizationItem = attrs.customizationItem
+
+    out << "${customizationItem.customization.label}"
+
+    def customizationPrice = 0
+
+    if (customizationItem.customization.chargeable) {
+      customizationPrice += customizationItem.customization.price
+    }
+
+    if (customizationItem.customization.type == CustomizationType.CHOICESET && customizationItem.customizationChoice.chargeable) {
+      customizationPrice += customizationItem.customizationChoice.price
+    }
+
+    if (customizationPrice > 0) {
+      out << " (${formatNumber(format: '\$0.00', number: customizationPrice)})"
+    }
+
+    out << ": ${attrs.customizationItem.value}"
+  }
+
+}
