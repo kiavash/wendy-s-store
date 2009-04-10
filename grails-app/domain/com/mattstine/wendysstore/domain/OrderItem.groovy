91e129f48c62f0e56a524b8c470ccf329366c4b1
mstine <matt@mattstine.com>
2009-04-09 20:01:55 -0500

    incorporate customizations into pricing

--- grails-app/domain/com/mattstine/wendysstore/domain/OrderItem.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/OrderItem.groovy
@@ -1,10 +1,42 @@
 package com.mattstine.wendysstore.domain
 
+import com.mattstine.wendysstore.domain.CustomizationItem
+import com.mattstine.wendysstore.domain.CustomizationType
+import com.mattstine.wendysstore.domain.Price
 import com.mattstine.wendysstore.domain.Product
 
 class OrderItem extends com.metasieve.shoppingcart.Shoppable {
 
   Product product
   Price price
-  
+
+  static hasMany = [customizationItems: CustomizationItem]
+
+  //This is necessary for inhertance hierarchies across an association (GRAILS-3588)
+  static mapping = { customizationItems(lazy: false) }
+
+  static transients = ['totalPrice']
+
+  BigDecimal getTotalPrice() {
+
+    BigDecimal totalPrice = 0.00
+
+    totalPrice += price.price
+
+    customizationItems.each {customizationItem ->
+      if (customizationItem.customization.type == CustomizationType.CHOICESET) {
+        if (customizationItem.customizationChoice.chargeable) {
+          totalPrice += customizationItem.customizationChoice.price
+        }
+      } else {
+        if (customizationItem.customization.chargeable) {
+          totalPrice += customizationItem.customization.price
+        }
+      }
+    }
+
+    totalPrice
+
+  }
+
 }
