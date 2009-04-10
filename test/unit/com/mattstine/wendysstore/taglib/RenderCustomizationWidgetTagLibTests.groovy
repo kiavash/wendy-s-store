af0237e9e9db221fd1b4de6faccf9bd0eecd1623
mstine <matt@mattstine.com>
2009-04-09 20:14:31 -0500

    test skeletons

---
+++ test/unit/com/mattstine/wendysstore/taglib/RenderCustomizationWidgetTagLibTests.groovy
@@ -0,0 +1,17 @@
+package com.mattstine.wendysstore.taglib
+
+import grails.test.*
+
+class RenderCustomizationWidgetTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
