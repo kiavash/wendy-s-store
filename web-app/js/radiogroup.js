704c2856de49f600e11fbd771c58011a057dff58
mstine <matt@mattstine.com>
2009-04-19 15:41:44 -0500

    js to get value of a radiogroup

---
+++ web-app/js/radiogroup.js
@@ -0,0 +1,22 @@
+/**
+* Returns the value of the selected radio button in the radio group, null if
+* none are selected, and false if the button group doesn't exist
+*
+* @param {radio Object} or {radio id} el
+* OR
+* @param {form Object} or {form id} el
+* @param {radio group name} radioGroup
+*/
+function $RF(el, radioGroup) {
+    if($(el).type && $(el).type.toLowerCase() == 'radio') {
+        var radioGroup = $(el).name;
+        var el = $(el).form;
+    } else if ($(el).tagName.toLowerCase() != 'form') {
+        return false;
+    }
+
+    var checked = $(el).getInputs('radio', radioGroup).find(
+        function(re) {return re.checked;}
+    );
+    return (checked) ? $F(checked) : null;
+}
