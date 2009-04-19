ddeb3c3e1f11d6fc92bf5d3cff40b33386471c4f
mstine <matt@mattstine.com>
2009-04-19 15:28:47 -0500

    add sortIndex...initialize to maxPlusOne....make Comparable and implement compareTo

--- grails-app/domain/com/mattstine/wendysstore/domain/Product.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/Product.groovy
@@ -1,5 +1,5 @@
 package com.mattstine.wendysstore.domain
-class Product {
+class Product implements Comparable {
   String name
   String description
   List prices
@@ -8,6 +8,7 @@ class Product {
   Image mediumImage
   Image thumbnailImage
   List customizations
+  Long sortIndex
 
   static constraints = {
     name(blank:false)
@@ -15,6 +16,7 @@ class Product {
     fullSizeImage(nullable:true)
     mediumImage(nullable:true)
     thumbnailImage(nullable:true)
+    sortIndex(nullable:true)
   }
 
   static belongsTo = [category:ProductCategory]
@@ -23,6 +25,21 @@ class Product {
 
   static mapping = {
     sort: "name"
+  }
+
+  int compareTo(obj) {
+    sortIndex.compareTo(obj.sortIndex)
+  }
+
+  //When inserting a new product, always assign it's initial sortIndex as the previous max sortIndex...(i.e. insert it at the
+  //bottom of the list).
+  def beforeInsert = {
+    def maxSortIndex = Product.executeQuery("select max(sortIndex) from Product")
+    if (maxSortIndex[0]) {
+      sortIndex = maxSortIndex[0] + 1
+    } else {
+      sortIndex = 1
+    }
   }
 
   
