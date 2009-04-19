e365f92bd729e4abea017edf12fd54a29b9eaf6e
mstine <matt@mattstine.com>
2009-04-19 15:28:12 -0500

    add constraints, break up phone number, get rid of zipPlusFour

--- grails-app/domain/com/mattstine/wendysstore/domain/Address.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/Address.groovy
@@ -1,10 +1,27 @@
 package com.mattstine.wendysstore.domain
+
 class Address {
+  String name
   String addressLineOne
   String addressLineTwo
   String city
-  String state
+  USState state
   String zipCode
-  String zipPlusFour
-  String phone
+  String areaCode
+  String phonePrefix
+  String phoneSuffix
+  Boolean defaultAddress = true
+
+  static belongsTo = [user:User]
+
+  static constraints = {
+    name(blank:false)
+    addressLineOne(blank:false)
+    city(blank:false)
+    zipCode(blank:false,matches:/\d{5}/)
+    areaCode(blank:false,matches:/\d{3}/)
+    phonePrefix(blank:false,matches:/\d{3}/)
+    phoneSuffix(blank:false,matches:/\d{4}/)
+    user(nullable:true)
+  }
 }
