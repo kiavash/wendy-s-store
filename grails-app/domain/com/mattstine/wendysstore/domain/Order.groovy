a27de0f56e7aaa3cf6df3717e2325038eeaad5d7
mstine <matt@mattstine.com>
2009-04-24 23:07:20 -0500

    fix compile error after removal of Customer

--- grails-app/domain/com/mattstine/wendysstore/domain/Order.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/Order.groovy
@@ -1,10 +1,8 @@
 package com.mattstine.wendysstore.domain
 class Order {
   static hasMany = [orderItem:OrderItem]
-  static belongsTo = [customer:Customer]
 
   Address shippingAddress
-  Address billingAddress
   Payment payment
 
    static mapping = {
