334630772e840ad2207cde0c9990fb37c36de909
mstine <matt@mattstine.com>
2009-04-27 00:40:10 -0500

    implement charge calculations

--- grails-app/domain/com/mattstine/wendysstore/domain/Order.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/Order.groovy
@@ -1,11 +1,53 @@
 package com.mattstine.wendysstore.domain
-class Order {
-  static hasMany = [orderItem:OrderItem]
+class Order implements Serializable {
+
+  static hasMany = [orderItems: OrderItem]
 
   Address shippingAddress
   Payment payment
+  CouponCode couponCode
 
-   static mapping = {
-      table 'orders'
-   }
+  static constraints = {
+    shippingAddress(nullable: true)
+    payment(nullable: true)
+    couponCode(nullable: true)
+  }
+
+  static mapping = {
+    table 'orders'
+  }
+
+  static transients = ['totalCharge', 'subTotal', 'amountOff']
+
+  def getTotalCharge() {
+    def subTotal = getSubtotal()
+    if (couponCode != null) {
+      return applyCouponCode(subTotal)
+    } else {
+      return subTotal
+    }
+  }
+
+  private def applyCouponCode(def total) {
+    return total - getAmountOff()
+  }
+
+  def getAmountOff() {
+    switch (couponCode.type) {
+      case CouponCodeType.PERCENT_OFF:
+        return total * couponCode.amount / 100
+      case CouponCodeType.DOLLARS_OFF:
+        return couponCode.amount
+    }
+  }
+
+  def getSubtotal() {
+    def subtotal = 0.00
+
+    orderItems.each() {item ->
+      subtotal += item.totalPrice * item.quantity
+    }
+
+    return subtotal
+  }
 }
