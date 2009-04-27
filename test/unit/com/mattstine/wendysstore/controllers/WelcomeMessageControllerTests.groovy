188973c3e39e28f073bb8884b6438b319f8cb442
mstine <matt@mattstine.com>
2009-04-27 00:44:00 -0500

    generated test harnesses

---
+++ test/unit/com/mattstine/wendysstore/controllers/WelcomeMessageControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.mattstine.wendysstore.controllers
+
+import grails.test.*
+
+class WelcomeMessageControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
