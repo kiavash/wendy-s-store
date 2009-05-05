4092142b00062af71d672cf3aa7af80fe578994c
mstine <matt@mattstine.com>
2009-05-04 22:11:58 -0500

    add phone number

--- grails-app/domain/com/mattstine/wendysstore/domain/User.groovy
+++ grails-app/domain/com/mattstine/wendysstore/domain/User.groovy
@@ -6,33 +6,41 @@ import com.mattstine.wendysstore.domain.Role
  * User domain class.
  */
 class User implements Serializable {
-	static transients = ['pass']
-	static hasMany = [authorities: Role, shippingAddresses:Address]
-	static belongsTo = Role
+  static transients = ['pass']
+  static hasMany = [authorities: Role, shippingAddresses: Address]
+  static belongsTo = Role
 
-	/** Username */
-	String username
-	/** User Real Name*/
-	String lastName
-    String firstName
-	/** MD5 Password */
-	String passwd
-	/** enabled */
-	boolean enabled
+  /** Username  */
+  String username
+  /** User Real Name */
+  String lastName
+  String firstName
+  /** MD5 Password  */
+  String passwd
+  /** enabled  */
+  boolean enabled
 
-	String email
+  String email
 
-	/** description */
-	String description = ''
+  /** description  */
+  String description = ''
 
-	/** plain password to create a MD5 password */
-	String pass = '[secret]'
+  /** plain password to create a MD5 password  */
+  String pass = '[secret]'
 
-	static constraints = {
-		username(blank: false, unique: true)
-		lastName(blank: false)
-        firstName(blank: false)
-		passwd(blank: false)
-		enabled()
-	}
+  //We want to capture a phone number for everybody
+  String areaCode
+  String phonePrefix
+  String phoneSuffix
+
+  static constraints = {
+    username(blank: false, unique: true)
+    lastName(blank: false)
+    firstName(blank: false)
+    passwd(blank: false)
+    areaCode(blank:false,matches:/\d{3}/)
+    phonePrefix(blank:false,matches:/\d{3}/)
+    phoneSuffix(blank:false,matches:/\d{4}/)
+    enabled()
+  }
 }
