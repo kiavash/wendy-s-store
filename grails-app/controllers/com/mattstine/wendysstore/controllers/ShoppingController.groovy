f68ada5cd07ec20d2fe429aee3a7d7d100ec2270
mstine <matt@mattstine.com>
2009-05-04 22:10:37 -0500

    integrate checkout portion of processOrder flow w/ paypal plugin

--- grails-app/controllers/com/mattstine/wendysstore/controllers/ShoppingController.groovy
+++ grails-app/controllers/com/mattstine/wendysstore/controllers/ShoppingController.groovy
@@ -2,6 +2,8 @@ package com.mattstine.wendysstore.controllers
 
 import com.mattstine.wendysstore.domain.*
 import org.codehaus.groovy.grails.plugins.springsecurity.Secured
+import org.grails.paypal.Payment
+import org.grails.paypal.PaymentItem
 
 class ShoppingController {
 
@@ -119,6 +121,88 @@ class ShoppingController {
       on("addShippingAddress") {
         [address: new Address(), userId: params.userId]
       }.to "addShippingAddress"
+
+      on("checkout").to "checkout"
+    }
+
+    checkout {
+      action { CheckoutCommand cmd ->
+        if (cmd.validate()) {
+
+          //Need to empty the shopping cart obviously - will refactor this out when I move
+          //to a shopping cart implementation based on webflow
+          shoppingCartService.checkOut()
+
+          def user = authenticateService.userDomain()
+          flow.order.user = User.get(user.id)
+
+          flow.order.deliveryMethod = cmd.deliveryMethod
+          if (CheckoutCommand.SHIP == cmd.deliveryMethod) {
+            flow.order.shippingAddress = Address.get(cmd.shippingAddress)
+          }
+
+          def payment = new Payment()
+          payment.buyerId = flow.order.user.id
+
+          flow.order.orderItems.each { orderItem ->
+            def paymentItem = new PaymentItem()
+
+            if (flow.order.couponCode) {
+              if (flow.order.couponCode.type == CouponCodeType.PERCENT_OFF) {
+                paymentItem.amount = orderItem.totalPrice - (orderItem.totalPrice * flow.order.couponCode.amount / 100)
+              }
+
+            //TODO: Handle dollar off coupons...lovely PayPal doesn't accept negative amount line items, so having
+            //TODO: to remove the discount on each item
+
+            } else {
+              paymentItem.amount = orderItem.totalPrice
+            }
+            paymentItem.quantity = orderItem.quantity
+            paymentItem.itemName = orderItem.product.name
+            paymentItem.itemNumber = orderItem.product.id
+            payment.addToPaymentItems(paymentItem)
+          }
+
+
+
+          flow.order.payment = payment.save()
+          flow.order.save(flush:true)
+
+          def redirectParamMap = [:]
+
+          redirectParamMap.returnController = 'shopping'
+          redirectParamMap.returnAction = 'success'
+          redirectParamMap.cancelController = 'shopping'
+          redirectParamMap.cancelAction = 'cancel'
+
+          redirectParamMap.transactionId = payment.transactionId
+          if (CheckoutCommand.LOCAL_PICKUP == cmd.deliveryMethod) {
+            redirectParamMap.noShipping = "true"
+          } else if (CheckoutCommand.SHIP == cmd.deliveryMethod) {
+            redirectParamMap.addressOverride = "true"
+            redirectParamMap.firstName = flow.order.user.firstName
+            redirectParamMap.lastName = flow.order.user.lastName
+            redirectParamMap.addressLineOne = flow.order.shippingAddress.addressLineOne
+            if (flow.order.shippingAddress.addressLineTwo) {
+              redirectParamMap.addressLineTwo = flow.order.shippingAddress.addressLineTwo
+            }
+            redirectParamMap.city = flow.order.shippingAddress.city
+            redirectParamMap.state = flow.order.shippingAddress.state
+            redirectParamMap.zipCode = flow.order.shippingAddress.zipCode
+            redirectParamMap.areaCode = flow.order.shippingAddress.areaCode
+            redirectParamMap.phonePrefix = flow.order.shippingAddress.phonePrefix
+            redirectParamMap.phoneSuffix = flow.order.shippingAddress.phoneSuffix
+          }
+
+           this.redirect(controller: 'paypal', action: 'uploadCart', params: redirectParamMap)
+
+
+        } else {
+          "fail"
+        }
+      }
+      on ("fail").to "reviewOrderDetails"
     }
 
     addShippingAddress {
@@ -147,25 +231,6 @@ class ShoppingController {
       on("success").to "reviewOrderDetails"
       on("fail").to "addShippingAddress"
     }
-
-  }
-
-  def checkout = {
-
-    def checkedOutItems = shoppingCartService.checkOut()
-
-    def url = new StringBuffer("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_cart&upload=1")
-    url << "&business=seller_1237686842_biz@mattstine.com"
-
-    def incrementalItemId = 1
-    checkedOutItems.each() {
-      url << "&item_name_${incrementalItemId}=${com.metasieve.shoppingcart.Shoppable.findByShoppingItem(it['item']).product.name}"
-      url << "&amount_${incrementalItemId}=${com.metasieve.shoppingcart.Shoppable.findByShoppingItem(it['item']).totalPrice}"
-      url << "&quantity_${incrementalItemId}=${it['qty']}"
-      incrementalItemId++
-    }
-
-    redirect(url: url)
 
   }
 
@@ -222,5 +287,34 @@ class ShoppingController {
     shoppingService.saveCategorySortOrder(categoryIds)
 
     render("Category sort order saved!")
+  }
+
+  def success = {
+    def payment = Payment.findByTransactionId(params.transactionId)
+    def order = Order.findByPayment(payment)
+
+    [payment:payment, order:order]
+  }
+
+  def cancel = { }
+
+}
+
+class CheckoutCommand {
+  static final LOCAL_PICKUP = 'Local Pickup'
+  static final SHIP = 'Ship'
+
+  String deliveryMethod
+  String shippingAddress
+
+  static constraints = {
+    deliveryMethod(blank: false)
+    shippingAddress(constraint: {
+      if (LOCAL_PICKUP == deliveryMethod) {
+        if (!shippingAddress || "" == shippingAddress.trim()) {
+          return "invalid.address.for.shipping"
+        }
+      }
+    })
   }
 }
