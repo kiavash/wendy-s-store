e673c2e237e825b3b556c9c1764657ebaca74ecf
mstine <mstine@L090047.local>
2009-07-20 22:44:24 -0500

    add token processing and email

--- grails-app/controllers/com/mattstine/wendysstore/controllers/ContactUsController.groovy
+++ grails-app/controllers/com/mattstine/wendysstore/controllers/ContactUsController.groovy
@@ -9,23 +9,33 @@ class ContactUsController {
 
   def index = {
     def config = recaptchaService.getRecaptchaConfig()
-    [publicKey:config.recaptcha.publicKey]
+    [publicKey: config.recaptcha.publicKey]
   }
 
   def sendMessage = {
-
     if (evaluateCaptcha(request.getRemoteAddr(), params.recaptcha_challenge_field, params.recaptcha_response_field)) {
+      withForm {
+        def msg = new ContactUs(params['msg'])
 
-      def msg = new ContactUs(params['msg'])
+        if (msg.save()) {
+          sendMail {
+            to "matt.stine@gmail.com"
+            from "contactus@duckduckgoosedesigns.com"
+            subject "Message from Contact Us Form at DuckDuckGooseDesigns.com!"
+            html g.render(template:"email", model: [message: msg]) 
+          }
 
-      if (msg.save()) {
-        render(template: 'messageSent')
-      } else {
-        render(template: 'errors', model: [message: msg])
+          render(template: 'messageSent')
+        } else {
+          render(template: 'errors', model: [message: msg])
+        }
+      }.invalidToken {
+        render(template: 'tokenError')
       }
-    } else {      
+    } else {
       render(template: 'recaptchaError')
     }
+
   }
 
   private def evaluateCaptcha(def remoteIp, def challenge, def response) {
